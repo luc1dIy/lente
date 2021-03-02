f08fc562259713eb247e2abbcdf345d2c22710d6
BuildTools <unconfigured@null.spigotmc.org>
2021-03-02 19:33:40 +0100

    changed requirements

--- data/flags.lua
+++ data/flags.lua
@@ -1,6 +1,6 @@
 return {
     order = { "libs/perip", "libs/settings" },
-    requirements = { "plethora:glasses", "plethora:scanner", "plethora:sensor", "plethora:kinetic" },
+    requirements = { "plethora:glasses", "plethora:introspection", "plethora:sensor", "plethora:kinetic", "plethora:keyboard" },
     side = "back",
     configPath = "config.json",
     mods = { "mods/leap" }
